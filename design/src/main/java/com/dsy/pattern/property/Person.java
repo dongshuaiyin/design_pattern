cf178c379334a38af9c335e850aab5b85a91a3ba
dongshuaiyin <m13269804793@163.com>
2020-04-02 21:19:06 +0800

    增加原型模式克隆方法

--- design/src/main/java/com/dsy/pattern/property/Person.java
+++ design/src/main/java/com/dsy/pattern/property/Person.java
@@ -1,4 +1,63 @@
 package com.dsy.pattern.property;
 
-public class Person {
+import lombok.Data;
+
+import java.io.*;
+import java.util.List;
+
+/**
+ *  测试类
+ *
+ *  原型模式克隆方法
+ *
+ */
+@Data
+public class Person implements Cloneable, Serializable {
+    private String name;
+    private String age;
+    private String sex;
+
+    private List<String> hobbies;
+
+    /**
+     *  浅克隆对象
+     * @return
+     * @throws CloneNotSupportedException
+     */
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    /**
+     * 深克隆对象
+     * @return
+     */
+    public Person deepClone() {
+
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(this);
+
+            ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(bis);
+
+            return (Person)ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", sex='" + sex + '\'' +
+                ", hobbies=" + hobbies +
+                '}';
+    }
 }
